18684acd60ba2cb15763832ce7756d2c6eb313e5
AhmedAbdo12345 <Ahmedabdo12377@gmail.com>
2023-10-27 00:01:16 +0200

    Design Home Screen

--- app/src/main/java/com/example/petfinder/presentation/MainActivity.kt
+++ app/src/main/java/com/example/petfinder/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.petfinder
+package com.example.petfinder.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.petfinder.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
