82ab740bb144c3dc6c622cab269135f2956d2245
AhmedAbdo12345 <Ahmedabdo12377@gmail.com>
2023-10-27 21:51:34 +0200

    Implement Remote Data Source Class

--- app/src/main/java/com/example/petfinder/data/source/AnimalRemoteDataSource.kt
+++ app/src/main/java/com/example/petfinder/data/source/AnimalRemoteDataSource.kt
@@ -1,8 +1,10 @@
 package com.example.petfinder.data.source
 
-import com.example.petfinder.data.model.AnimalsResponse
+import com.example.petfinder.data.model.token.AccessTokenResponse
+import com.example.petfinder.data.model.animal.AnimalsResponse
 
 interface AnimalRemoteDataSource {
 
     suspend fun getAllAnimals(): AnimalsResponse
+    suspend fun getToken(): AccessTokenResponse
 }
--- app/src/main/java/com/example/petfinder/data/source/AnimalRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/petfinder/data/source/AnimalRemoteDataSourceImpl.kt
@@ -1,6 +1,8 @@
 package com.example.petfinder.data.source
 
-import com.example.petfinder.data.model.AnimalsResponse
+import android.util.Log
+import com.example.petfinder.data.model.token.AccessTokenResponse
+import com.example.petfinder.data.model.animal.AnimalsResponse
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -8,7 +10,15 @@ import kotlinx.coroutines.withContext
 class AnimalRemoteDataSourceImpl(private val apiService: ApiService, private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO) : AnimalRemoteDataSource {
     override suspend fun getAllAnimals(): AnimalsResponse {
        return withContext(ioDispatcher){
-           apiService.getAnimals()
+           Log.d("zxcv", "getAllAnimals: "+getToken().accessToken)
+           apiService.getAnimals("Bearer "+getToken().accessToken)
+
        }
+    }
+
+    override suspend fun getToken(): AccessTokenResponse {
+      return withContext(ioDispatcher){
+          apiService.getAccessToken("client_credentials","Vx9UJgGvTkHK9K0YtEcqQbpbsjlzePGyrbu3dk0TVTYHVc59Vf","dOSATgMmqiaWyMCzhCmhBasZJSQgMPtXiSKXfBSi")
+      }
     }
 }
