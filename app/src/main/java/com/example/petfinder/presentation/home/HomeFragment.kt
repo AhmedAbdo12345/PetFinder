cc019fe98a867fb1ffd99f7c59719a3125f9b6fa
AhmedAbdo12345 <Ahmedabdo12377@gmail.com>
2023-10-28 03:38:46 +0200

    fetch all animal types from Api and display them in tab layout

--- app/src/main/java/com/example/petfinder/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/petfinder/presentation/home/HomeFragment.kt
@@ -1,27 +1,32 @@
 package com.example.petfinder.presentation.home
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.example.petfinder.R
 import com.example.petfinder.ResponseState
+import com.example.petfinder.data.model.types.Type
+import com.example.petfinder.data.model.types.TypeResponse
 import com.example.petfinder.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayout
 import kotlinx.coroutines.launch
 
 
 class HomeFragment : Fragment() {
-private lateinit var binding : FragmentHomeBinding
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var tabLayout: TabLayout
+
     private val homeViewModel by viewModels<HomeViewModel> {
         HomeViewModel.Factory
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -40,27 +45,89 @@ private lateinit var binding : FragmentHomeBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        tabLayout = binding.tablayoutTypes
+
         homeViewModel.getAnimals()
-
-        viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                homeViewModel.animals.collect{
-                  when(it){
-                      is ResponseState.OnLoading ->{
-
-                      }
-                      is ResponseState.OnSuccess ->{
+        collectDataOfAnimalsFromViewModel()
 
 
-                      }
-                      is ResponseState.OnError -> {
-
-                      }
-
-                  }
-                }
-
-            }}
+        homeViewModel.getTypes()
+        collectDataOfTypessFromViewModel()
 
     }
+
+    fun collectDataOfAnimalsFromViewModel() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                homeViewModel.animals.collect {
+                    when (it) {
+                        is ResponseState.OnLoading -> {
+
+                        }
+
+                        is ResponseState.OnSuccess -> {
+                        }
+
+                        is ResponseState.OnError -> {
+
+                        }
+
+                    }
+                }
+
+
+            }
+        }
+    }
+
+    fun collectDataOfTypessFromViewModel() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                homeViewModel.types.collect {
+                    when (it) {
+                        is ResponseState.OnLoading -> {
+
+                        }
+
+                        is ResponseState.OnSuccess -> {
+                            tabLayout.removeAllTabs()
+
+                            val firstTab = tabLayout.newTab()
+                                .setText("All")
+
+                            tabLayout.addTab(firstTab)
+                            for (x in 0 until it.response.types.size){
+                                val typeObj = it.response.types[x]
+                                val myTab = tabLayout.newTab()
+                                    .setText(typeObj.name)
+                                tabLayout.addTab(myTab)
+
+                            }
+                        }
+
+                        is ResponseState.OnError -> {
+
+                        }
+
+                    }
+                }
+
+
+            }
+        }
+    }
+
+    fun getAnimalListForEachTab(){
+        /*tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+
+                val animalType = tab.tag as Type
+                collectDataOfAnimalsFromViewModel()
+
+
+            }})*/
+    }
+
 }
+
