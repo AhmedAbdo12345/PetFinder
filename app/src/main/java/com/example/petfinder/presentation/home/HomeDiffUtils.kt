cc3ea1d9b985252fe5ce7a14f97ee20a4ec29a7d
AhmedAbdo12345 <Ahmedabdo12377@gmail.com>
2023-10-28 03:32:21 +0200

    create class HomeDiffUtils

--- app/src/main/java/com/example/petfinder/presentation/home/HomeDiffUtils.kt
+++ app/src/main/java/com/example/petfinder/presentation/home/HomeDiffUtils.kt
@@ -1,4 +1,15 @@
 package com.example.petfinder.presentation.home
 
-class HomeDiffUtils {
+import androidx.recyclerview.widget.DiffUtil
+import com.example.petfinder.data.model.animal.Animal
+
+class HomeDiffUtils  : DiffUtil.ItemCallback<Animal>(){
+
+    override fun areItemsTheSame(oldItem: Animal, newItem: Animal): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Animal, newItem: Animal): Boolean {
+        return oldItem == newItem
+    }
 }
